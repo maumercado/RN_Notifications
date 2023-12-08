b28bc3c708689a384754535609b3e71e036cde47
Mauricio Mercado <mau@maumercado.com>
2023-12-08 09:42:46 -0800

    Scheduled notifications working

--- App.js
+++ App.js
@@ -1,10 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+
+  useEffect(() => {
+    const subs = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification, 'received');
+    })
+    return () => {
+      subs.remove();
+    }
+  }, []);
+
+  async function scheduleNotificationHandler() {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      console.log('Failed to get permission for notifications')
+      return;
+    }
+
+    try {
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'My first local notification',
+          body: 'This is the first local notification we are sending!',
+          data: { userName: 'Mau' },
+        },
+        trigger: {
+          seconds: 4,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Schedule notification" onPress={scheduleNotificationHandler}/>
       <StatusBar style="auto" />
     </View>
   );
